5e20c5a36097b54b6252de7525da7bc3780198c2
expo <expoworld112@gmail.com>
2025-10-03 13:44:39 +0530

    update2

--- Coding4u-Frontend/config.js
+++ Coding4u-Frontend/config.js
@@ -1,16 +1,16 @@
-// export const API = "https://expoworld-backend.vercel.app/api";
-// export const BACKEND = "https://expoworld-backend.vercel.app";
-// export const DOMAIN = "https://expoworld-frontend.vercel.app";
+export const API = "https://expo-ashen-two.vercel.app/api";
+export const BACKEND = "https:expo-ashen-two.vercel.app";
+export const DOMAIN = "https://expoworld-frontend.vercel.app";
 
 
 
-export const API = 'http://localhost:8000/api';
-export const DOMAIN = "http://localhost:3000";
-export const BACKEND = "http://localhost:8000";
+// export const API = 'http://localhost:8000/api';
+// export const DOMAIN = "http://localhost:3000";
+// export const BACKEND = "http://localhost:8000";
 
 
 
 export const APP_NAME = "EXPOWORLD.COM";
 export const DISQUS_SHORTNAME = "my-cms-7";
 export const APP_DESCRIPTION = "Expo World WEBSITE IS THE  PLATFORM WHERE USER CAN SHARE THE RESOURSE."
-export const MY_API = "https://expoworld-backend.vercel.app";
+export const MY_API = "https://expo-ashen-two.vercel.app";
