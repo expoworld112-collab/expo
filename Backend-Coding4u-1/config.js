b2f9f30a9c86aa85f3e47b36f2441d782ccf8919
expo <expoworld112@gmail.com>
2025-10-06 15:53:43 +0530

    kjkj

--- Backend-Coding4u-1/config.js
+++ Backend-Coding4u-1/config.js
@@ -1,5 +1,5 @@
 // export const FRONTEND = "https://expoworld-frontend.vercel.app";
-export const FRONTEND = "https://coding4u-frontend.vercel.app//";
+export const FRONTEND = "https://coding4u-frontend.vercel.app";
 
 
 
