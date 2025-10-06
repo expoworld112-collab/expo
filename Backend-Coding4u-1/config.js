566e9ad7ba739fa19f1003d1c4a6640efde390fd
expo <expoworld112@gmail.com>
2025-10-06 14:05:57 +0530

    qwerty

--- Backend-Coding4u-1/config.js
+++ Backend-Coding4u-1/config.js
@@ -1,6 +1,6 @@
 // export const FRONTEND = "https://expoworld-frontend.vercel.app";
-export const FRONTEND = "https://expo-xjj9.vercel.app/";
-";
+export const FRONTEND = "https://coding4u-frontend.vercel.app//";
+
 
 
 // export const FRONTEND = "http://localhost:3000";
