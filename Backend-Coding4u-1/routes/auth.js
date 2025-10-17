7a8eb9e3b8b433fa5548f61424b7ef2181763417
expo <expoworld112@gmail.com>
2025-10-17 17:42:36 +0530

    cc

--- Backend-Coding4u-1/routes/auth.js
+++ Backend-Coding4u-1/routes/auth.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { signup, signin, signout, requireSignin, forgotPassword, resetPassword, preSignup, activateAccount } from "../controllers/auth.js";
+import { signup, signin, signout, forgotPassword, resetPassword, preSignup, activateAccount } from "../controllers/auth.js";
 import { runvalidation } from "../validators/index.js";
 import { check } from "express-validator";
 
